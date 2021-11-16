e712f045e43514ffeeb28ccaa4b8e0fc2ba65282
nadia-pisarello <pisarello.nads@gmail.com>
2021-11-16 16:39:44 -0300

    ejericicio 4 resuelto

--- EjerciciosComplementariosLevel3/Ejercicioss4.java
+++ EjerciciosComplementariosLevel3/Ejercicioss4.java
@@ -1,3 +1,20 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Ejercicioss4 {
-    
+    public static void main(String[] args) {
+        List<Integer> palabras = List.of(1, 2, 4, 4, 4);
+        List<Integer> factorial = palabras.stream()
+            .map(p -> Ejercicioss4.factorizando(p))
+            .distinct()
+            .collect(Collectors.toList());
+        System.out.println(factorial);        
+    }
+    public static Integer factorizando (int numero) {
+        if (numero==0){
+          return 1;
+        }else{
+          return numero * factorizando(numero-1);
+        }
+    }
 }
